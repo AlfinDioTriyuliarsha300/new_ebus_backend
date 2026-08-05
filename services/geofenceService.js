4d391dd358c55587be4a61ea3e9384d643ab141d
AlfinDioTriyuliarsha300 <lahkonmenungso@gmail.com>
2026-08-05 08:31:21 +0700

    Update geofenceService.js

--- services/geofenceService.js
+++ services/geofenceService.js
@@ -13,7 +13,6 @@ exports.checkBusGeofence = async (
   latitude,
   longitude
 ) => {
-  throw new Error("INI TEST RAILWAY");
   try {
     console.log("================================");
     console.log("CHECK GEOFENCE DIPANGGIL");
